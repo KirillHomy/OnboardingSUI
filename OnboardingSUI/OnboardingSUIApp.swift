c5fa895494dcc6a753ed61c0479e78d3c3713acc
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-24 18:24:21 +0200

    Initial Commit

---
+++ OnboardingSUI/OnboardingSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingSUIApp.swift
+//  OnboardingSUI
+//
+//  Created by Kirill Khomicevich on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
